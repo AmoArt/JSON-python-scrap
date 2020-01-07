6b4d75ff4799e4008e3baf4028db055010b7397c
AmoArt <52915248+AmoArt@users.noreply.github.com>
2020-01-07 14:01:43 +0000

    Add files via upload

---
+++ runJSONscrapper.py
@@ -0,0 +1,14 @@
+﻿import json
+
+
+with open('text.txt', 'rb') as json_file:
+    
+    data = json.load(json_file)
+
+    for p in data:
+        try:
+            print(p['b']+"\n")
+        except:
+            print("")
+
+
